41bb6b239e3cf2ccf1f9e34ac9c68508c0989a26
Mike Cheung <mikewlcheung@nus.edu.sg>
2023-05-31 08:30:02 +0000

    version 0.2

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-[![Build Status](https://travis-ci.org/mikewlcheung/symsem.svg?branch=master)](https://travis-ci.org/mikewlcheung/symsem)
+[![R build status](https://github.com/mikewlcheung/symsem/workflows/R-CMD-check/badge.svg)](https://github.com/mikewlcheung/symsem/actions)
+[![cran version](http://www.r-pkg.org/badges/version/symSEM)](https://cran.r-project.org/package=symSEM)
+[![Monthly Downloads](http://cranlogs.r-pkg.org/badges/symSEM)](https://cranlogs.r-pkg.org/badges/symSEM)
+[![Total Downloads](http://cranlogs.r-pkg.org/badges/grand-total/symSEM)](https://cranlogs.r-pkg.org/badges/grand-total/symSEM)
+[![Rdoc](http://www.rdocumentation.org/badges/version/symSEM)](https://www.rdocumentation.org/packages/symSEM)
 
 # symSEM
 Symbolic Computation for Structural Equation Models
+
+The symSEM package uses the [caracas](https://cran.r-project.org/package=caracas) package, which depends on the [SymPy](https://www.sympy.org/) library, for the symbolic computations.
+
+The stable version can be installed from CRAN by:
+```
+install.packages("symSEM")
+```
+
+The developmental version can be installed from GitHub by:
+```
+## Install remotes package if it has not been installed yet
+# install.packages("remotes")
+
+remotes::install_github("mikewlcheung/symsem")
+```
+
+
+# Example
+
+## Compute a symbolic model-implied covariance and correlation matrices
+
+```
+library(symSEM)
+
+## A regression model
+model <- "y ~ b1*x1 + b2*x2
+          ## Covariance between x1 and x2
+          x1 ~~ x2
+          ## Means
+          y ~ b0*1
+          x1 ~ m1*1
+          x2 ~ m2*1"
+
+## Convert it into a RAM speculation
+RAM <- metaSEM::lavaan2RAM(model)
+
+## Implied covariance matrix and mean structure
+impliedS(RAM, corr=FALSE)
+
+## Implied correlation matrix
+impliedS(RAM, corr=TRUE)
+```
--- tests/testthat/test_utilities.R
+++ tests/testthat/test_utilities.R
@@ -1,42 +1,12 @@
-context("Checking utility functions")
-
-test_that("sym() works correctly", {
-  ## Test one element
-  A1 <- "log(sin(x))"
-  expect_identical(sym(A1), Ryacas::ysym("Ln(Sin(x))"))
-
-  ## Test two elements
-  A2 <- c("log(x)", "cos(y)")
-  expect_identical(sym(A2), Ryacas::ysym(c("Ln(x)", "Cos(y)")))
-
-  ## Test a column vector
-  A3 <- matrix(c("tan(a1)", "acos(a2)"), ncol=1)
-  expect_identical(sym(A3), 
-                   Ryacas::ysym(matrix(c("Tan(a1)", "ArcCos(a2)"), 
-                   ncol=1)))
-
-  ## Test a matrix
-  A4 <- matrix(paste0("a", 1:6), ncol=2)
-  expect_identical(sym(A4), Ryacas::ysym(A4))              
-})
-
-test_that("as.matrix() works correctly", {
-
-  A1 <- matrix(c(1, 2, 3, "a", "b", "c"), ncol=2, nrow=3)
-  A2  <- Ryacas::ysym(A1)
-  expect_identical(A1, as.matrix(A2))
-
-  A3 <- matrix(c(1, 2, 3, "exp(a)", "log(b)", "sqrt(c)"), ncol=2, nrow=3)
-  A4 <- matrix(c(1, 2, 3, "Exp(a)", "Ln(b)", "Sqrt(c)"), ncol=2, nrow=3)
-  expect_identical(A3, as.matrix(Ryacas::ysym(A4)))
-})
+## Skip the tests in cran as it is difficult to install the required environments, such as python and sympy.
+skip_on_cran()
 
 test_that("impliedS() works correctly", {
 
     model1 <- "y ~ c*x + b*m
-               m ~ a*x
+               m ~ a_a*x
                x ~~ 1*x
-               y ~ b0*1
+               y ~ b_0*1
                m ~ m0*1
                x ~ x0*1"
     
@@ -45,9 +15,9 @@ test_that("impliedS() works correctly", {
     S1a <- impliedS(RAM1, corr=TRUE)
     S1b <- impliedS(RAM1, corr=FALSE)
 
-    R1a <- list(Sigma = structure(c("1", "a*c+b", "c+b*a",
-                                    "b+a*c", "1", "a",
-                                    "c+b*a", "a", "1"),
+    R1a <- list(Sigma = structure(c("1", "a_a*c + b", "a_a*b + c",
+                                    "a_a*c + b", "1", "a_a",
+                                    "a_a*b + c", "a_a", "1"),
                                   .Dim = c(3L, 3L),
                                   .Dimnames = list(c("y", "m", "x"),
                                                    c("y", "m", "x"))),
@@ -55,16 +25,13 @@ test_that("impliedS() works correctly", {
                                .Dim = c(1L, 3L),
                                .Dimnames = list("1", c("y", "m", "x"))),
                 corr = TRUE)
-    R1b <- list(Sigma = structure(c("yWITHy+mWITHm*b^2+(c+b*a)^2", "mWITHm*b+a*(c+b*a)", "c+b*a",
-                                    "b*mWITHm+(c+b*a)*a", "mWITHm+a^2", "a",
-                                    "c+b*a", "a", "1"),
-                                  .Dim = c(3L, 3L),
-                                  .Dimnames = list(c("y", "m", "x"),
-                                                   c("y", "m", "x"))),
-                mu = structure(c("b0+m0*b+x0*(c+b*a)",
-                                 "m0+x0*a","x0"),
-                               .Dim = c(1L, 3L),
-                               .Dimnames = list("1", c("y", "m", "x"))),
+    R1b <- list(Sigma = structure(c("b^2*mWITHm + yWITHy + (a_a*b + c)^2", 
+                                    "a_a*(a_a*b + c) + b*mWITHm", "a_a*b + c", "a_a*(a_a*b + c) + b*mWITHm", 
+                                    "a_a^2 + mWITHm", "a_a", "a_a*b + c", "a_a", "1"),
+                                  dim = c(3L, 3L),
+                                  dimnames = list(c("y", "m", "x"), c("y", "m", "x"))),
+                mu = structure(c("b*m0 + b_0 + x0*(a_a*b + c)", "a_a*x0 + m0", "x0"),
+                               dim = c(1L, 3L), dimnames = list("1", c("y", "m", "x"))),
                 corr = FALSE)
 
     expect_identical(S1a, R1a)
